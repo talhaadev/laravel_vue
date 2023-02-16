1482817ef8526b5a5036884d5a2f8c791dc70f85
FrankLucas <52176945+muneebtariq10@users.noreply.github.com>
2023-02-17 01:21:41 +0500

    Working

---
+++ resources/js/components/Welcome.vue
@@ -0,0 +1,8 @@
+<template>
+    <div class="container mt-5">
+        <div class="col-12 text-center">
+            <h1>Laravel Vue Crud App</h1>
+            <a href="https://github.com/talhaadev/laravel_vue" target="_blank">Visit For Project Repository</a>
+        </div>
+    </div>
+</template>
