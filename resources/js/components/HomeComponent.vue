fc6c02b3826e4cfa3ed471b44a3097ea654177e1
FrankLucas <52176945+muneebtariq10@users.noreply.github.com>
2023-02-17 00:05:50 +0500

    Setup

---
+++ resources/js/components/HomeComponent.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="">
+                <div class="card">
+                    <div class="card-header">Home Component</div>
+
+                    <div class="card-body">
+                        I'm Home component.
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
